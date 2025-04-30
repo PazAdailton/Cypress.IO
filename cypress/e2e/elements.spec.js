75c34157d669d6e4be3676b1ee4596a9668ba166
Adailton <paazadailton@gmail.com>
2025-04-30 13:56:24 -0300

    atualizações

--- cypress/e2e/elements.spec.js
+++ cypress/e2e/elements.spec.js
@@ -73,7 +73,7 @@ describe('Work with basic elements', () => {
         cy.get('#formComidaPizza').should('not.be.checked')
     })
 
-    it('Combo', () => {
+    it.only('Combo', () => {
         cy.get('[data-test="dataEscolaridade"]')
         .select('2o grau completo')
         .should('have.value', '2graucomp')
@@ -82,14 +82,38 @@ describe('Work with basic elements', () => {
         .select('1graucomp')
         .should('have.value', '1graucomp')
 
-        //TODO validar as opções do combo
+        //DOING validar as opções do combo
+        cy.get('[data-test="dataEscolaridade"] option')
+        .should('have.length', 8)
+        cy.get('[data-test="dataEscolaridade"] option').then($arr => {
+            const values = []
+            $arr.each(function() {
+              values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(['Superior', 'Mestrado'])
+        })
     })
 
     it.only('Combo Múltiplo', () => {
         cy.get('[data-testid="dataEsportes"]')
         .select(['natacao', 'Corrida', 'nada'])
 
+        //cy.get('[data-testid="dataEsportes"]')
+        //.should('have.value','natacao', 'Corrida', 'nada' )
+
         //TODO validar opções selecionadas do combo múltiplo
+        cy.get('[data-testid="dataEsportes"]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+
+        cy.get('[data-testid="dataEsportes"]')
+        .invoke('val')
+        .should('eql', ['natacao', 'Corrida', 'nada'])
     })
+
+
+
+   
 })
 
---
+++ cypress/e2e/sync.spec.js
@@ -0,0 +1,69 @@
+/// < reference types="cypress"/>
+
+const { expect } = require("chai");
+
+
+describe('Esperas..', () => {
+
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+    })
+
+    before(() => {
+        cy.reload();
+    })
+    it('Deve aguardar elemento estar disponível', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funcion')
+            .should('have.value', 'funcion')
+    })
+
+    it('Uso do find', () => {
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+            /* cy.get('#lista li')
+                .find('span')
+                .should('contain', 'Item 2') */~
+            cy.get('#lista li span')
+                .should('contain', 'Item 2')
+    })
+    it('Uso do timeout', () => {
+        // cy.get('#buttonDelay').click()
+        //cy.get('#novoCampo', {timeout: 1000}).should('exist')
+/*         cy.get('#buttonListDOM').click()
+        //cy.wait(5000)
+        cy.get('#lista li span', {timeout: 30000})
+            .should('contain', 'Item 2'); */
+
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span', {timeout: 3000})
+        .should('have.length', 1)
+        cy.get('#lista li span')
+        .should('have.length', 2)
+    })
+
+    it.only('Click retry', () => {
+        cy.get('#buttonCount')
+        .click()
+        .click()
+        .should('have.value', 111)
+    })
+
+    it.only('Should vs Then', () => {
+        //cy.get('#buttonListDOM').click()
+        //cy.get('#buttonListDOM').should($el => { ignora o return porque sempre retorna o mem elemento que recebeu
+        cy.get('#buttonListDOM').then($el => {
+            //.should('have.length', 1) //elemento cypress
+            //console.log($el)
+            expect($el).to.have.length(1) 
+            return 2
+        }).and('eq', 2)
+        
+    })
+
+})
